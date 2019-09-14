1994f409549182b0cb0ae3ddf8acb72c032d3601
dagui07 <53511606+dawang07@users.noreply.github.com>
2019-09-14 17:39:18 +0800

    大体文件结构完成
    把所需要的文件夹建好了

---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import Vue from "vue";
+import Router from "vue-router";
+
+//使用router包
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: "/login",
+      name: "login",
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ "@/views/login")
+    }
+  ]
+});
